e5217d387600ca373346ffd26641dd1f1b750b9a
SergioP20 <114087307+SergioP20@users.noreply.github.com>
2022-10-17 20:25:58 -0500

    Most commented layout and Controller

---
+++ src/main/java/com/softserve/controller/home/ArticlesController.java
@@ -0,0 +1,13 @@
+package com.softserve.controller.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ArticlesController {
+
+	@GetMapping("/article")
+	public String articlesView() {
+		return "configurations/mostcommented";
+	}
+}
