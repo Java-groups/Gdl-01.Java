568258bd9de69e81f0e19eebb22c209473dc42da
Ivan Caballero <gabrielivn12@gmail.com>
2022-11-06 19:26:38 -0600

    Swagger implementation: Dependecies for swagger added and SpringFoxConfig file created. in application.yml enable bean-definition-overriding

--- src/main/java/com/softserve/config/WebMvcConfig.java
+++ src/main/java/com/softserve/config/WebMvcConfig.java
@@ -5,12 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
-@Configuration 
+@Configuration
+@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
 	 
@@ -40,6 +43,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		
 		
 		registry.addInterceptor(localeInterceptor).addPathPatterns("/*");
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html")
+				.addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**")
+				.addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 	
 }
