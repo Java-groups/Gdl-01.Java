b7f55204c3525bcfe3fea73601d0d576fe4ab883
Luis Angel Abrego Hererra <anl_32@live.com.mx>
2022-11-11 08:16:19 -0600

    Update SocialController.java

--- src/main/java/com/softserve/controller/home/api/SocialController.java
+++ src/main/java/com/softserve/controller/home/api/SocialController.java
@@ -3,20 +3,13 @@ package com.softserve.controller.home.api;
 import java.util.Collections;
 import java.util.Map;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class SocialApplication {
+public class SocialController {
 
     @RequestMapping("/user")
     public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
