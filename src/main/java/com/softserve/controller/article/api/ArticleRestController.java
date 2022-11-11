848ac78c05c225effa2eee4a936d5c0d77fcd4e9
jjcastellanos <jjcastellanos@mticonsultoria.com>
2022-11-10 22:28:18 -0600

    Fix jwt validations

--- src/main/java/com/softserve/controller/article/api/ArticleRestController.java
+++ src/main/java/com/softserve/controller/article/api/ArticleRestController.java
@@ -4,6 +4,7 @@ import com.softserve.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,11 +17,13 @@ public class ArticleRestController {
     @Autowired
     private ArticleService articleService;
 
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @PostMapping(value = "/new", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<?> saveArticle(@RequestParam Map<String, Object> json, @RequestParam("articleImage") MultipartFile articleImage){
         return this.articleService.saveArticle(json, articleImage);
     }
 
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @GetMapping("/load")
     public ResponseEntity<?> readAllSubCategory(){
         return this.articleService.loadNewArticleContentApi();
